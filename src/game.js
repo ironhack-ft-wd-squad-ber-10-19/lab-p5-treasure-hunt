69871e29f10c612346a41a3bf06d0a55d791bdf7
Pascal Hanke <pascal.hanke@yahoo.de>
2019-10-24 22:17:09 +0200

    done

--- src/game.js
+++ src/game.js
@@ -1,7 +1,437 @@
+let squareWidth = WIDTH / 10;
+
+let margin = squareWidth / 5;
+
 class Game {
   drawGrid() {
     // Iteration 1
     // Draw the grid
     // https://p5js.org/reference/#/p5/line
+
+    /* let width = 500; */
+    push();
+    strokeWeight(3);
+    stroke(255);
+    for (let i = 0; i <= 10; i++) {
+      line(0, squareWidth * i, WIDTH, squareWidth * i);
+      line(squareWidth * i, 0, squareWidth * i, HEIGHT);
+    }
+    pop();
   }
+}
+
+
+
+
+
+class Player {
+  constructor(col, row) {
+    this.col = col;
+    this.row = row;
+    this.score = 0;
+  }
+
+  draw() {
+    push();
+    tint(255, 100, 0, 255);
+    image(imgPlayer, this.col + margin / 2, this.row + margin / 2, squareWidth - margin, squareWidth - margin);
+    pop();
+  }
+
+  moveUp() {
+    if (this.row >= 0 + squareWidth) {
+      this.row = this.row - squareWidth;
+      if (this.col === treasure.col && this.row === treasure.row) {
+        this.score++;
+        treasure.setRandomPosition();
+      }
+
+      if (this.col === player2.col && this.row === player2.row) {
+        this.row = this.row + squareWidth;
+      }
+      if (this.col === player3.col && this.row === player3.row) {
+        this.row = this.row + squareWidth;
+      }
+      if (this.col === player4.col && this.row === player4.row) {
+        this.row = this.row + squareWidth;
+      }
+      /*       image(imgPlayer, this.row- squareWidth, this.col, 50, 50); */
+    }
+  }
+  moveDown() {
+    if (this.row <= HEIGHT - 2 * squareWidth) {
+      this.row = this.row + squareWidth;
+      if (this.col === treasure.col && this.row === treasure.row) {
+        this.score++;
+        treasure.setRandomPosition();
+      }
+
+      if (this.col === player2.col && this.row === player2.row) {
+        this.row = this.row - squareWidth;
+      }
+      if (this.col === player3.col && this.row === player3.row) {
+        this.row = this.row - squareWidth;
+      }
+      if (this.col === player4.col && this.row === player4.row) {
+        this.row = this.row - squareWidth;
+      }
+      /*      image(imgPlayer, this.row+ squareWidth, this.col, 50, 50); */
+    }
+  }
+
+  moveLeft() {
+    if (this.col >= 0 + squareWidth) {
+      this.col = this.col - squareWidth;
+      if (this.col === treasure.col && this.row === treasure.row) {
+        this.score++;
+        treasure.setRandomPosition();
+      }
+      if (this.col === player2.col && this.row === player2.row) {
+        this.col = this.col + squareWidth;
+      }
+      if (this.col === player3.col && this.row === player3.row) {
+        this.col = this.col + squareWidth;
+      }
+      if (this.col === player4.col && this.row === player4.row) {
+        this.col = this.col + squareWidth;
+      }
+    }
+  }
+
+  moveRight() {
+    if (this.col <= WIDTH - 2 * squareWidth)
+      this.col = this.col + squareWidth;
+    if (this.col === treasure.col && this.row === treasure.row) {
+      this.score++;
+      treasure.setRandomPosition();
+    }
+    if (this.col === player2.col && this.row === player2.row) {
+      this.col = this.col - squareWidth;
+    }
+    if (this.col === player3.col && this.row === player3.row) {
+      this.col = this.col - squareWidth;
+    }
+    if (this.col === player4.col && this.row === player4.row) {
+      this.col = this.col - squareWidth;
+    }
+
+  }
+
+}
+
+class Treasure extends Player {
+  constructor(col, row) {
+    super(col, row)
+  }
+  draw() {
+    push();
+    tint(255, 255, 255)
+    image(imgTreasure, this.col - margin / 2, this.row - margin / 2, squareWidth + margin, squareWidth + margin);
+    pop();
+
+  }
+
+  setRandomPosition() {
+    this.col = Math.floor(Math.random() * 10) * squareWidth;
+    this.row = Math.floor(Math.random() * 10) * squareWidth;
+
+    /*     console.log(this.col);
+        console.log(this.row); */
+  }
+}
+
+
+class Player2 extends Player {
+  constructor(col, row) {
+    super(col, row)
+    this.col = col;
+    this.row = row;
+    this.score = 0;
+  }
+  draw() {
+    push();
+    tint(100, 255, 200, 255)
+    image(imgPlayer2, this.col + margin / 2, this.row + margin / 2, squareWidth - margin, squareWidth - margin);
+    pop();
+  }
+
+
+  moveUp() {
+    if (this.row >= 0 + squareWidth) {
+      this.row = this.row - squareWidth;
+      if (this.col === treasure.col && this.row === treasure.row) {
+        this.score++;
+        treasure.setRandomPosition();
+      }
+
+      if (this.col === player.col && this.row === player.row) {
+        this.row = this.row + squareWidth;
+      }
+      if (this.col === player3.col && this.row === player3.row) {
+        this.row = this.row + squareWidth;
+      }
+      if (this.col === player4.col && this.row === player4.row) {
+        this.row = this.row + squareWidth;
+      }
+      /*       image(imgPlayer, this.row- squareWidth, this.col, 50, 50); */
+    }
+  }
+  moveDown() {
+    if (this.row <= HEIGHT - 2 * squareWidth) {
+      this.row = this.row + squareWidth;
+      if (this.col === treasure.col && this.row === treasure.row) {
+        this.score++;
+        treasure.setRandomPosition();
+      }
+
+      if (this.col === player.col && this.row === player.row) {
+        this.row = this.row - squareWidth;
+      }
+      if (this.col === player3.col && this.row === player3.row) {
+        this.row = this.row - squareWidth;
+      }
+      if (this.col === player4.col && this.row === player4.row) {
+        this.row = this.row - squareWidth;
+      }
+      /*      image(imgPlayer, this.row+ squareWidth, this.col, 50, 50); */
+    }
+  }
+
+  moveLeft() {
+    if (this.col >= 0 + squareWidth) {
+      this.col = this.col - squareWidth;
+      if (this.col === treasure.col && this.row === treasure.row) {
+        this.score++;
+        treasure.setRandomPosition();
+      }
+      if (this.col === player.col && this.row === player.row) {
+        this.col = this.col + squareWidth;
+      }
+      if (this.col === player3.col && this.row === player3.row) {
+        this.col = this.col + squareWidth;
+      }
+      if (this.col === player4.col && this.row === player4.row) {
+        this.col = this.col + squareWidth;
+      }
+    }
+  }
+
+  moveRight() {
+    if (this.col <= WIDTH - 2 * squareWidth)
+      this.col = this.col + squareWidth;
+    if (this.col === treasure.col && this.row === treasure.row) {
+      this.score++;
+      treasure.setRandomPosition();
+    }
+    if (this.col === player.col && this.row === player.row) {
+      this.col = this.col - squareWidth;
+    }
+    if (this.col === player3.col && this.row === player3.row) {
+      this.col = this.col - squareWidth;
+    }
+    if (this.col === player4.col && this.row === player4.row) {
+      this.col = this.col - squareWidth;
+    }
+
+  }
+
+}
+
+
+
+class Player3 {
+  constructor(col, row) {
+    this.col = col;
+    this.row = row;
+    this.score = 0;
+  }
+
+
+  draw() {
+    push();
+    tint(0, 100, 255, 255);
+    image(imgPlayer3, this.col + margin / 2, this.row + margin / 2, squareWidth - margin, squareWidth - margin);
+    pop();
+  }
+
+  moveUp() {
+    if (this.row >= 0 + squareWidth) {
+      this.row = this.row - squareWidth;
+      if (this.col === treasure.col && this.row === treasure.row) {
+        this.score++;
+        treasure.setRandomPosition();
+      }
+
+      if (this.col === player.col && this.row === player.row) {
+        this.row = this.row + squareWidth;
+      }
+      if (this.col === player2.col && this.row === player2.row) {
+        this.row = this.row + squareWidth;
+      }
+      if (this.col === player4.col && this.row === player4.row) {
+        this.row = this.row + squareWidth;
+      }
+      /*       image(imgPlayer, this.row- squareWidth, this.col, 50, 50); */
+    }
+  }
+  moveDown() {
+    if (this.row <= HEIGHT - 2 * squareWidth) {
+      this.row = this.row + squareWidth;
+      if (this.col === treasure.col && this.row === treasure.row) {
+        this.score++;
+        treasure.setRandomPosition();
+      }
+
+      if (this.col === player.col && this.row === player.row) {
+        this.row = this.row - squareWidth;
+      }
+      if (this.col === player2.col && this.row === player2.row) {
+        this.row = this.row - squareWidth;
+      }
+      if (this.col === player4.col && this.row === player4.row) {
+        this.row = this.row - squareWidth;
+      }
+      /*      image(imgPlayer, this.row+ squareWidth, this.col, 50, 50); */
+    }
+  }
+
+  moveLeft() {
+    if (this.col >= 0 + squareWidth) {
+      this.col = this.col - squareWidth;
+      if (this.col === treasure.col && this.row === treasure.row) {
+        this.score++;
+        treasure.setRandomPosition();
+      }
+      if (this.col === player.col && this.row === player.row) {
+        this.col = this.col + squareWidth;
+      }
+      if (this.col === player2.col && this.row === player2.row) {
+        this.col = this.col + squareWidth;
+      }
+      if (this.col === player4.col && this.row === player4.row) {
+        this.col = this.col + squareWidth;
+      }
+    }
+  }
+
+  moveRight() {
+    if (this.col <= WIDTH - 2 * squareWidth)
+      this.col = this.col + squareWidth;
+    if (this.col === treasure.col && this.row === treasure.row) {
+      this.score++;
+      treasure.setRandomPosition();
+    }
+    if (this.col === player.col && this.row === player.row) {
+      this.col = this.col - squareWidth;
+    }
+    if (this.col === player2.col && this.row === player2.row) {
+      this.col = this.col - squareWidth;
+    }
+    if (this.col === player4.col && this.row === player4.row) {
+      this.col = this.col - squareWidth;
+    }
+
+  }
+
+
+
+}
+
+class Player4 {
+  constructor(col, row) {
+    this.col = col;
+    this.row = row;
+    this.score = 0;
+  }
+
+
+  draw() {
+    push();
+    tint(210, 255, 0, 255);
+    image(imgPlayer4, this.col + margin / 2, this.row + margin / 2, squareWidth - margin, squareWidth - margin);
+    pop();
+  }
+
+  moveUp() {
+    if (this.row >= 0 + squareWidth) {
+      this.row = this.row - squareWidth;
+      if (this.col === treasure.col && this.row === treasure.row) {
+        this.score++;
+        treasure.setRandomPosition();
+      }
+
+      if (this.col === player.col && this.row === player.row) {
+        this.row = this.row + squareWidth;
+      }
+
+      if (this.col === player2.col && this.row === player2.row) {
+        this.row = this.row + squareWidth;
+      }
+      if (this.col === player3.col && this.row === player3.row) {
+        this.row = this.row + squareWidth;
+      }
+      /*       image(imgPlayer, this.row- squareWidth, this.col, 50, 50); */
+    }
+  }
+  moveDown() {
+    if (this.row <= HEIGHT - 2 * squareWidth) {
+      this.row = this.row + squareWidth;
+      if (this.col === treasure.col && this.row === treasure.row) {
+        this.score++;
+        treasure.setRandomPosition();
+      }
+
+      if (this.col === player.col && this.row === player.row) {
+        this.row = this.row - squareWidth;
+      }
+      if (this.col === player2.col && this.row === player2.row) {
+        this.row = this.row - squareWidth;
+      }
+      if (this.col === player3.col && this.row === player3.row) {
+        this.row = this.row - squareWidth;
+      }
+      /*      image(imgPlayer, this.row+ squareWidth, this.col, 50, 50); */
+    }
+  }
+
+  moveLeft() {
+    if (this.col >= 0 + squareWidth) {
+      this.col = this.col - squareWidth;
+      if (this.col === treasure.col && this.row === treasure.row) {
+        this.score++;
+        treasure.setRandomPosition();
+      }
+      if (this.col === player.col && this.row === player.row) {
+        this.col = this.col + squareWidth;
+      }
+      if (this.col === player2.col && this.row === player2.row) {
+        this.col = this.col + squareWidth;
+      }
+      if (this.col === player3.col && this.row === player3.row) {
+        this.col = this.col + squareWidth;
+      }
+    }
+  }
+
+  moveRight() {
+    if (this.col <= WIDTH - 2 * squareWidth)
+      this.col = this.col + squareWidth;
+    if (this.col === treasure.col && this.row === treasure.row) {
+      this.score++;
+      treasure.setRandomPosition();
+    }
+    if (this.col === player.col && this.row === player.row) {
+      this.col = this.col - squareWidth;
+    }
+    if (this.col === player2.col && this.row === player2.row) {
+      this.col = this.col - squareWidth;
+    }
+    if (this.col === player3.col && this.row === player3.row) {
+      this.col = this.col - squareWidth;
+    }
+
+  }
+
+
+
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,143 @@
 const game = new Game();
+const player = new Player(0, 0);
+const treasure = new Treasure();
+const player2 = new Player2(WIDTH - squareWidth, WIDTH - squareWidth);
+const player3 = new Player3(WIDTH - squareWidth, 0);
+const player4 = new Player4(0, WIDTH - squareWidth);
+
+let imgPlayer;
+let imgPlayer2;
+let imgPlayer3;
+let imgPlayer4;
+let imgTreasure;
+let imgTexture;
+let pos;
+let pos2;
+
+
+
+function preload() {
+  imgPlayer = loadImage("/assets/character-down.png");
+  imgPlayer2 = loadImage("/assets/character-up.png");
+  imgPlayer3 = loadImage("/assets/character-left.png");
+  imgPlayer4 = loadImage("/assets/character-right.png");
+  imgTreasure = loadImage("/assets/treasure.png");
+  imgTexture = loadImage("/assets/abstract-wooden-seamless-texture-background_23-2148283204 (1).jpg");
+
+}
+
 
 function setup() {
   let canvas = createCanvas(WIDTH, HEIGHT);
   canvas.parent("canvas");
+  drawTreasure();
 }
 
+
+
 function draw() {
+  background("white");
+  push();
+  tint(150, 200, 255, 80);
+
+  background(imgTexture);
+  pop();
+  fill("red");
   game.drawGrid();
+  player.draw();
+  player2.draw();
+  player3.draw();
+  player4.draw();
+  treasure.draw()
+  updateScores();
+
+
+
+};
+
+
+setInterval(function () {
+  Math.ceil(Math.random() * 3);
+  pos = (Math.ceil(Math.random() * 4))
+  console.log(pos);
+  if (pos === 1) { //move left
+    player3.moveLeft();
+    player3.draw();
+  } else if (pos === 2) { //move right
+    player3.moveRight();
+    player3.draw();
+  } else if (pos === 3) { //move down 
+    player3.moveDown();
+    player3.draw();
+  } else if (pos === 4) { //move up
+    player3.moveUp();
+    player3.draw();
+  }
+
+}, 100);
+
+setInterval(function () {
+  Math.ceil(Math.random() * 3);
+  pos2 = (Math.ceil(Math.random() * 4))
+  console.log(pos);
+  if (pos2 === 1) { //move left
+    player4.moveLeft();
+    player4.draw();
+  } else if (pos2 === 2) { //move right
+    player4.moveRight();
+    player4.draw();
+  } else if (pos2 === 3) { //move down 
+    player4.moveDown();
+    player4.draw();
+  } else if (pos2 === 4) { //move up
+    player4.moveUp();
+    player4.draw();
+  }
+
+}, 100);
+
+
+
+function keyPressed() {
+  console.log("hello")
+  //will detect keycode of any of the keys
+  if (keyCode === 37) { //move left
+    player.moveLeft();
+    player.draw();
+  } else if (keyCode === 39) { //move right
+    player.moveRight();
+    player.draw();
+  } else if (keyCode === 40) { //move down 
+    player.moveDown();
+    player.draw();
+  } else if (keyCode === 38) { //move up
+    player.moveUp();
+    player.draw();
+  } else if (keyCode === 65) { //move left
+    player2.moveLeft();
+    player2.draw();
+  } else if (keyCode === 68) { //move right
+    player2.moveRight();
+    player2.draw();
+  } else if (keyCode === 83) { //move down 
+    player2.moveDown();
+    player2.draw();
+  } else if (keyCode === 87) { //move up
+    player2.moveUp();
+    player2.draw();
+  }
+
+}
+
+function updateScores() {
+
+  document.querySelector(".player1 span").innerText = player.score;
+  document.querySelector(".player2 span").innerText = player2.score;
+  document.querySelector(".player3 span").innerText = player3.score;
+  document.querySelector(".player4 span").innerText = player4.score;
+
+}
+
+function drawTreasure() {
+  treasure.setRandomPosition();
 }
