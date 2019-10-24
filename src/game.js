3555b112723f42da9118637bfbe491291f773e37
Fernando Araujo <feraraujofilho@gmail.com>
2019-10-24 18:35:22 +0200

    done

--- src/game.js
+++ src/game.js
@@ -1,7 +1,66 @@
 class Game {
   drawGrid() {
+    for (let row = 0; row <= 1000; row += HEIGHT / 10) {
+      for (let col = 0; col <= 1000; col += WIDTH / 10) {
+        line(0, row, 1000, row);
+        line(col, 0, col, 1000);
+      }
+    }
     // Iteration 1
     // Draw the grid
     // https://p5js.org/reference/#/p5/line
+  }
+}
+
+class Player {
+  constructor(col, row) {
+    this.col = col;
+    this.row = row;
+  }
+
+  moveUp() {
+    clear();
+    this.row -= 100;
+  }
+
+  moveDown() {
+    clear();
+    this.row += 100;
+  }
+
+  moveLeft() {
+    clear();
+    this.col -= 100;
+  }
+
+  moveRight() {
+    clear();
+    this.col += 100;
+  }
+
+  draw() {
+    image(img, this.col, this.row, 100, 100);
+  }
+}
+
+class Treasure {
+  constructor() {
+    this.col = 0;
+    this.row = 0;
+  }
+
+  draw() {
+    image(
+      img2,
+      this.col * SQUARE_SIDE,
+      this.row * SQUARE_SIDE,
+      SQUARE_SIDE,
+      SQUARE_SIDE
+    );
+  }
+
+  setRandomPosition() {
+    this.row = Math.floor(random(1,10));
+    this.col = Math.floor(random(1,10));
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,34 @@
 const game = new Game();
+const player = new Player(0, 0);
+const treasure = new Treasure();
+
+function preload() {
+  console.log("Preload");
+  // loadImage(path to the actual image)
+  img = loadImage("assets/character-down.png");
+  img2 = loadImage("assets/treasure.png");
+}
 
 function setup() {
   let canvas = createCanvas(WIDTH, HEIGHT);
+  treasure.setRandomPosition();
   canvas.parent("canvas");
 }
 
 function draw() {
   game.drawGrid();
+  player.draw();
+  treasure.draw();
+}
+
+function keyPressed() {
+  if (keyCode === 37) {
+    player.moveLeft();
+  } else if (keyCode === 39) {
+    player.moveRight();
+  } else if (keyCode === 40) {
+    player.moveDown();
+  } else if (keyCode === 38) {
+    player.moveUp();
+  }
 }
