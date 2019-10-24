b424193d77042484e14ced640f1b5594f6619846
Patrick Cho <pck.cho@gmail.com>
2019-10-25 00:07:38 +0200

    lab near complete

--- src/game.js
+++ src/game.js
@@ -1,7 +1,121 @@
+let charUp;
+let charDown;
+let charLeft;
+let charRight;
+let treasure;
+
+function preload() {
+  charUp = loadImage("/assets/character-up.png")
+  charDown = loadImage("/assets/character-down.png")
+  charLeft = loadImage("/assets/character-left.png")
+  charRight = loadImage("/assets/character-right.png")
+  treasure = loadImage("/assets/treasure.png")
+}
+
 class Game {
   drawGrid() {
     // Iteration 1
     // Draw the grid
     // https://p5js.org/reference/#/p5/line
+    for (var x = 0; x <= WIDTH; x += WIDTH / 10) { // DRAWING THE VERTICAL LINES
+      stroke(0);
+      strokeWeight(1);
+      line(x, 0, x, HEIGHT);
+    }
+    for (var y = 0; y <= HEIGHT; y += HEIGHT / 10) { // DRAWING THE HORIZONTAL LINES
+      stroke(0);
+      strokeWeight(1);
+      line(0, y, WIDTH, y);
+    }
+  }
+  collisionDetection() {
+    if (player1.row === treasure1.row && player1.col === treasure1.col) {
+      treasure1.setRandomPosition();
+      document.querySelector('.play1Score').innerText++;
+    }
+    if (player2.row === treasure1.row && player2.col === treasure1.col) {
+      treasure1.setRandomPosition();
+      document.querySelector('.play2Score').innerText++;
+    }
+  }
+}
+
+class Player1 {
+  constructor(row, col) {
+    this.row = row;
+    this.col = col;
+  };
+  moveLeft() {
+    this.row -= WIDTH / 10;;
+  }
+  moveRight() {
+    this.row += WIDTH / 10;
+  }
+  moveUp() {
+    this.col -= HEIGHT / 10;
+  }
+  moveDown() {
+    this.col += HEIGHT / 10;
+  }
+  draw() {
+    if (keyCode === 37) {
+      // move left
+      image(charLeft, this.row, this.col, 50, 50);
+    } else if (keyCode === 39) {
+      // move right
+      image(charRight, this.row, this.col, 50, 50);
+    } else if (keyCode === 38) {
+      // move up
+      image(charUp, this.row, this.col, 50, 50);
+    } else {
+      image(charDown, this.row, this.col, 50, 50);
+    }
+  }
+}
+
+class Treasure {
+  constructor(row, col) {
+    this.row = row;
+    this.col = col;
+  }
+  drawTreasure() {
+    image(treasure, this.row, this.col, WIDTH / 10, HEIGHT / 10)
+  }
+  setRandomPosition() {
+    this.row = (Math.floor(random() * 10) * WIDTH / 10);
+    this.col = (Math.floor(random() * 10) * HEIGHT / 10);
+  }
+}
+
+class Player2 {
+  constructor(row, col) {
+    this.row = row;
+    this.col = col;
+  };
+  moveLeft() {
+    this.row -= WIDTH / 10;;
+  }
+  moveRight() {
+    this.row += WIDTH / 10;
+  }
+  moveUp() {
+    this.col -= HEIGHT / 10;
+  }
+  moveDown() {
+    this.col += HEIGHT / 10;
+  }
+  draw() {
+    if (keyCode === 65) {
+      // move left
+      image(charLeft, this.row, this.col, 50, 50);
+    } else if (keyCode === 68) {
+      // move right
+      image(charRight, this.row, this.col, 50, 50);
+    } else if (keyCode === 87) {
+      // move up
+      image(charUp, this.row, this.col, 50, 50);
+    } else {
+      image(charDown, this.row, this.col, 50, 50);
+    }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,50 @@
 const game = new Game();
+const player1 = new Player1(0, 0);
+const player2 = new Player2(WIDTH - WIDTH / 10, 0)
+const treasure1 = new Treasure()
 
 function setup() {
   let canvas = createCanvas(WIDTH, HEIGHT);
   canvas.parent("canvas");
+  treasure1.setRandomPosition();
 }
 
 function draw() {
+  background('pink');
   game.drawGrid();
+  player1.draw();
+  player2.draw();
+  treasure1.drawTreasure();
+  game.collisionDetection();
+}
+
+function keyPressed() {
+  // PLAYER 1's KEYS
+  if (keyCode === 37 && player1.row > 0) {
+    // move left
+    player1.moveLeft();
+  } else if (keyCode === 39 && player1.row < (WIDTH - WIDTH / 10)) {
+    // move right
+    player1.moveRight();
+  } else if (keyCode === 38 && player1.col > 0) {
+    // move up
+    player1.moveUp();
+  } else if (keyCode === 40 && player1.col < (HEIGHT - HEIGHT / 10)) {
+    // move down
+    player1.moveDown();
+  }
+  // PLAYER 2's KEYS
+  if (keyCode === 65 && player2.row > 0) {
+    // move left
+    player2.moveLeft();
+  } else if (keyCode === 68 && player2.row < (WIDTH - WIDTH / 10)) {
+    // move right
+    player2.moveRight();
+  } else if (keyCode === 87 && player2.col > 0) {
+    // move up
+    player2.moveUp();
+  } else if (keyCode === 83 && player2.col < (HEIGHT - HEIGHT / 10)) {
+    // move down
+    player2.moveDown();
+  }
 }
