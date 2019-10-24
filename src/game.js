6bb5978af912eede1bd168730a28e9334e496292
Stephen Coley <stephencoley@Stephens-MacBook-Air.local>
2019-10-24 22:02:55 +0200

    done

--- src/game.js
+++ src/game.js
@@ -1,7 +1,65 @@
 class Game {
   drawGrid() {
-    // Iteration 1
-    // Draw the grid
-    // https://p5js.org/reference/#/p5/line
+    line(100, 0, 100, 1000);
+    line(200, 0, 200, 1000);
+    line(300, 0, 300, 1000);
+    line(400, 0, 400, 1000);
+    line(500, 0, 500, 1000);
+    line(600, 0, 600, 1000);
+    line(700, 0, 700, 1000);
+    line(800, 0, 800, 1000);
+    line(900, 0, 900, 1000);
+    line(1000, 0, 1000, 1000);
+    line(0, 0, 0, 1000);
+    //rows
+    line(1000, 100, 0, 100);
+    line(1000, 200, 0, 200);
+    line(1000, 300, 0, 300);
+    line(1000, 400, 0, 400);
+    line(1000, 500, 0, 500);
+    line(1000, 600, 0, 600);
+    line(1000, 700, 0, 700);
+    line(1000, 800, 0, 800);
+    line(1000, 900, 0, 900);
+    line(1000, 1000, 0, 1000);
+    line(1000, 0, 0, 0);
   }
 }
+
+class Player {
+  constructor(col, row) {
+    this.col = col;
+    this.row = row;
+  }
+  moveUp() {
+    this.row -= 100;
+  }
+
+  moveDown() {
+    this.row += 100;
+  }
+
+  moveRight() {
+    this.col += 100;
+  }
+
+  moveLeft() {
+    this.col -= 100;
+  }
+
+  draw() {
+    image(img, this.col, this.row, 100, 100);
+  }
+}
+
+class Treasure {
+  constructor(col, row){
+    this.col=col;
+    this.row=row;
+  }
+
+    draw(){
+      image(img2, this.col, this.row, 100,100)    
+    }
+}
+
--- src/main.js
+++ src/main.js
@@ -1,10 +1,47 @@
 const game = new Game();
+const player = new Player(0, 500);
+const treasure = new Treasure(0, 100);
 
+let img;
+let img2;
+
+function preload() {
+  img = loadImage("../assets/character-down.png");
+  img2 = loadImage("../assets/treasure.png");
+}
+
+//setup
 function setup() {
-  let canvas = createCanvas(WIDTH, HEIGHT);
+  let canvas = createCanvas(1000, 1000);
   canvas.parent("canvas");
 }
 
+let x = 100;
+let y = 100;
+let side = 100;
+
+//draw
 function draw() {
+  clear();
   game.drawGrid();
+  player.draw();
+  treasure.draw();
+}
+
+//KEY PRESS LRUD
+function keyPressed() {
+  console.log(keyCode);
+  if (keyCode === 37) {
+    // move left
+    player.col -= side; // x = x - 20;
+  } else if (keyCode === 39) {
+    // move right
+    player.col += side;
+  } else if (keyCode === 40) {
+    // move down
+    player.row += side; // y = y + 20;
+  } else if (keyCode === 38) {
+    // move up
+    player.row -= side; // y = y - 20;
+  }
 }
