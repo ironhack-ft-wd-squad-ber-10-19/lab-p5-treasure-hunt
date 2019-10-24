2bdd7c66c06c5bc0e4db76000aa75387c122663d
Davide Bernocchi <55143818+davideberno@users.noreply.github.com>
2019-10-24 20:00:53 +0200

    done

--- src/game.js
+++ src/game.js
@@ -1,7 +1,60 @@
 class Game {
   drawGrid() {
-    // Iteration 1
-    // Draw the grid
-    // https://p5js.org/reference/#/p5/line
+    square(0, 0, 1000);
+    line(0, 100, 1000, 100);
+    line(0, 200, 1000, 200);
+    line(0, 300, 1000, 300);
+    line(0, 400, 1000, 400);
+    line(0, 500, 1000, 500);
+    line(0, 600, 1000, 600);
+    line(0, 700, 1000, 700);
+    line(0, 800, 1000, 800);
+    line(0, 900, 1000, 900);
+    line(100, 0, 100, 1000);
+    line(200, 0, 200, 1000);
+    line(300, 0, 300, 1000);
+    line(400, 0, 400, 1000);
+    line(500, 0, 500, 1000);
+    line(600, 0, 600, 1000);
+    line(700, 0, 700, 1000);
+    line(800, 0, 800, 1000);
+    line(900, 0, 900, 1000);
+  }
+}
+
+class Player {
+  constructor(x, y) {
+    this.x = Math.floor(Math.random() * 9) * 100;
+    this.y = Math.floor(Math.random() * 9) * 100;
+  }
+
+  moveUp() {
+    this.y -= SQUARE_SIDE;
+  }
+  moveDown() {
+    this.y += SQUARE_SIDE;
+  }
+  moveLeft() {
+    this.x -= SQUARE_SIDE;
+  }
+  moveRight() {
+    this.x += SQUARE_SIDE;
+  }
+  draw() {
+    image(img, this.x, this.y, 100, 100);
+  }
+}
+
+class Treasure {
+  constructor(x, y) {
+    this.x = 0;
+    this.y = 0;
+  }
+  setRandomPosition() {
+    this.x = Math.floor(Math.random() * 9) * 100;
+    this.y = Math.floor(Math.random() * 9) * 100;
+  }
+  drawTreasure() {
+    image(treasureImg, this.x, this.y, 100, 100);
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,72 @@
 const game = new Game();
+const player = new Player();
+const player2 = new Player();
+const treasure = new Treasure();
+const score1 = document.querySelector("#score1");
+const score2 = document.querySelector("#score2");
+
+let img;
+let treasureImg;
+
+function preload() {
+  img = loadImage("assets/character-down.png");
+  treasureImg = loadImage("assets/treasure.png");
+}
 
 function setup() {
   let canvas = createCanvas(WIDTH, HEIGHT);
   canvas.parent("canvas");
+  treasure.setRandomPosition();
 }
 
 function draw() {
   game.drawGrid();
+
+  player.draw();
+  player2.draw();
+  treasure.drawTreasure();
+}
+
+function keyPressed() {
+  if (keyCode === 37 && player.x > 0) {
+    player.moveLeft();
+  } else if (keyCode === 39 && player.x < 900) {
+    player.moveRight();
+  } else if (keyCode === 40 && player.y < 900) {
+    player.moveDown();
+  } else if (keyCode === 38 && player.y > 0) {
+    player.moveUp();
+  }
+  if (keyCode === 65 && player2.x > 0) {
+    player2.moveLeft();
+  } else if (keyCode === 68 && player2.x < 900) {
+    player2.moveRight();
+  } else if (keyCode === 83 && player2.y < 900) {
+    player2.moveDown();
+  } else if (keyCode === 87 && player2.y > 0) {
+    player2.moveUp();
+  }
+}
+
+function keyReleased() {
+  addScore();
+  translateTreasure();
+}
+
+function addScore() {
+  if (player.x === treasure.x && player.y === treasure.y) {
+    score1.innerHTML = Number(score1.innerHTML) + 100;
+  } else if (player2.x === treasure.x && player2.y === treasure.y) {
+    score2.innerHTML = Number(score2.innerHTML) + 100;
+  }
+}
+
+function translateTreasure() {
+  if (player.x === treasure.x && player.y === treasure.y) {
+    treasure.x = Math.floor(Math.random() * 9) * 100;
+    treasure.y = Math.floor(Math.random() * 9) * 100;
+  } else if (player2.x === treasure.x && player2.y === treasure.y) {
+    treasure.x = Math.floor(Math.random() * 9) * 100;
+    treasure.y = Math.floor(Math.random() * 9) * 100;
+  }
 }
