5a51f960e43b87a00b6fbb341250fbc6ebd44b73
fabianschu <48454910+fabianschu@users.noreply.github.com>
2019-10-24 21:51:04 +0200

    done

--- src/game.js
+++ src/game.js
@@ -3,5 +3,66 @@ class Game {
     // Iteration 1
     // Draw the grid
     // https://p5js.org/reference/#/p5/line
+    
+    background('cyan');
+    let w = 100;
+    let h = 100;
+
+    for (let i = 0; i < 1000; i=i+100) {
+      for (let j = 0; j < 1000; j=j+100) {
+        rect(i, j, w, h);
+      }
+    }
+    
   }
+}
+
+class Player {
+  
+  constructor(col, row) {
+    this.column = col;
+    this.row = row;
+  }
+  
+  moveUp() {
+    if (this.row > 0) {
+      this.row -= 1;
+    }
+  }
+
+  moveDown() {
+    if (this.row < 10) {
+      this.row += 1;
+    }
+  }
+
+  moveRight() {
+    if (this.column < 10) {
+      this.column += 1;
+    }
+  }
+
+  moveLeft() {
+    if (this.column > 0) {
+      this.column -= 1;
+    }
+  }
+  
+  draw() {
+    image(img, this.column * 100, this.row * 100, 100, 100);
+  }
+  
+}
+
+class Treasure {
+  constructor() {
+    this.column = Math.floor(Math.random() * 10);
+    this.row = Math.floor(Math.random() * 10);
+  }
+
+  draw() {
+    image(imgTreasure, this.column * 100, this.row * 100, 100, 100)
+  }
+
+
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,39 @@
 const game = new Game();
+let player = new Player(0, 0);
+let treasure = new Treasure();
+
+let img;
+let imgTreasure;
 
 function setup() {
   let canvas = createCanvas(WIDTH, HEIGHT);
   canvas.parent("canvas");
 }
 
+function preload() {
+  img = loadImage("./assets/character-down.png");
+  imgTreasure = loadImage("./assets/treasure.png");
+}
+
 function draw() {
   game.drawGrid();
+  player.draw();
+  treasure.draw();
+}
+
+function keyPressed() {
+  console.log(keyCode);
+  if (keyCode === 37) {
+    // move left
+    player.moveLeft(); //x = x - 20
+  } else if (keyCode === 39) {
+    //move right
+    player.moveRight();
+  } else if (keyCode === 40) {
+    //move down
+    player.moveDown();
+  } else if (keyCode === 38) {
+    //move up
+    player.moveUp();
+  } 
 }
