7d2af281b4fd0609226284c48578351064d5f0d6
Johnny Burke <johnalbertburke@gmail.com>
2020-01-16 17:46:33 +0100

    done

--- src/game.js
+++ src/game.js
@@ -3,5 +3,30 @@ class Game {
     // Iteration 1
     // Draw the grid
     // https://p5js.org/reference/#/p5/line
+    //For (var BEGIN; END; INTERVAL){
+    //DO SOMETHING }
+    line(0, 0, 0, 1000);
+    line(100, 0, 100, 1000);
+    line(200, 0, 200, 1000);
+    line(300, 0, 300, 1000);
+    line(400, 0, 400, 1000);
+    line(500, 0, 500, 1000);
+    line(600, 0, 600, 1000);
+    line(700, 0, 700, 1000);
+    line(800, 0, 800, 1000);
+    line(900, 0, 900, 1000);
+    line(1000, 0, 1000, 1000);
+    //Create Line X-Achse
+    line(0, 0, 1000, 0);
+    line(0, 100, 1000, 100);
+    line(0, 200, 1000, 200);
+    line(0, 300, 1000, 300);
+    line(0, 400, 1000, 400);
+    line(0, 500, 1000, 500);
+    line(0, 600, 1000, 600);
+    line(0, 700, 1000, 700);
+    line(0, 800, 1000, 800);
+    line(0, 900, 1000, 900);
+    line(0, 1000, 1000, 1000);
   }
 }
