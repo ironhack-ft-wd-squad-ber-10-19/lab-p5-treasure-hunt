e9c10dd9c1d5b526d09230390d181868bea7ccac
jonathansaudhof <jonathan@jonathansaudhof.de>
2020-01-16 18:10:31 +0100

    done

--- src/game.js
+++ src/game.js
@@ -1,7 +1,91 @@
 class Game {
+  /*  constructor(width, height, squares){
+    this.width = width;
+    this.height = height; 
+    this.squares = squares; 
+  }
+ */
+
   drawGrid() {
     // Iteration 1
     // Draw the grid
     // https://p5js.org/reference/#/p5/line
+
+    //horizontal lines
+    for (let y = 0; y <= WIDTH; y += SQUARE_SIDE) {
+      line(0, y, WIDTH, y);
+    }
+
+    //vertical lines
+    for (let x = 0; x <= HEIGHT; x += SQUARE_SIDE) {
+      line(x, 0, x, HEIGHT);
+    }
+  }
+}
+
+class Player {
+  constructor(col, row) {
+    this.col = col;
+    this.row = row;
+    this.score = 0;
+  }
+
+  moveUp() {
+    this.row -= 1;
+    this.activePlayerImage = this.playerImageUp;
+  }
+  moveDown() {
+    this.row += 1;
+    this.activePlayerImage = this.playerImageDown;
+  }
+  moveLeft() {
+    this.col -= 1;
+    this.activePlayerImage = this.playerImageLeft;
+  }
+  moveRight() {
+    this.col += 1;
+    this.activePlayerImage = this.playerImageRight;
+    // console.log(this.playerImageRight);
+  }
+
+  addImages(image) {
+    this.playerImageUp = image.up;
+    this.playerImageDown = image.down;
+    this.playerImageLeft = image.left;
+    this.playerImageRight = image.right;
+
+    this.activePlayerImage = image.down;
+  }
+
+  draw() {
+    //clear();
+    // place the preloaded image on X and Y depending on the rows
+
+    this.activePlayerImage.resize(SQUARE_SIDE, SQUARE_SIDE);
+    image(
+      this.activePlayerImage,
+      this.col * SQUARE_SIDE,
+      this.row * SQUARE_SIDE,
+    );
+  }
+}
+
+class Treasure {
+  constructor() {
+    this.col = 0;
+    this.row = 0;
+  }
+  addImage(image) {
+    this.treasureImage = image;
+  }
+
+  setRandomPosition() {
+    this.col = Math.floor(Math.random() * (WIDTH / SQUARE_SIDE));
+    this.row = Math.floor(Math.random() * (HEIGHT / SQUARE_SIDE));
+  }
+
+  drawTreasure() {
+    this.treasureImage.resize(SQUARE_SIDE, SQUARE_SIDE);
+    image(this.treasureImage, this.col * SQUARE_SIDE, this.row * SQUARE_SIDE);
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,78 @@
 const game = new Game();
+const player1 = new Player(0, 0);
+const treasure = new Treasure();
 
 function setup() {
   let canvas = createCanvas(WIDTH, HEIGHT);
-  canvas.parent("canvas");
+  canvas.parent('canvas');
 }
 
 function draw() {
+  clear();
+
   game.drawGrid();
+  player1.draw();
+  treasure.drawTreasure();
+  updateScoreBoard();
+  hunted();
+  // keyPressed();
+}
+
+function hunted() {
+  if (player1.col === treasure.col && player1.row === treasure.row) {
+    // set new position until treasure place != playersplace
+    while (player1.col === treasure.col && player1.row === treasure.row) {
+      treasure.setRandomPosition();
+    }
+    player1.score += 100;
+    updateScoreBoard(player1);
+  }
+}
+
+function updateScoreBoard() {
+  //update player 1
+
+  const $player1Score = document.getElementById('player1Score');
+  const $player2Score = document.getElementById('player2Score');
+
+  //update player 2
+
+  $player1Score.innerHTML = player1.score;
+  //$player2Score.innerHTML = player2.score;
+}
+
+function keyPressed() {
+  if (keyCode === 38) {
+    player1.moveUp();
+  }
+  if (keyCode === 40) {
+    player1.moveDown();
+  }
+  if (keyCode === 37) {
+    player1.moveLeft();
+  }
+  if (keyCode === 39) {
+    player1.moveRight();
+  }
+}
+
+function preload() {
+  let player1Image = {
+    down: loadImage('assets/character-down.png'),
+    up: loadImage('assets/character-up.png'),
+    left: loadImage('assets/character-left.png'),
+    right: loadImage('assets/character-right.png'),
+  };
+
+  /*   let player2Image = {
+    down: loadImage('assets/character2-down.png'),
+    up: loadImage('assets/character2-up.png'),
+    left: loadImage('assets/character2-left.png'),
+    right: loadImage('assets/character2-left.png'),
+  }; */
+
+  player1.addImages(player1Image);
+  treasure.setRandomPosition();
+  treasure.addImage(loadImage('assets/treasure.png'));
+  //player1 = new Player(0, 0, playerImage);
 }
