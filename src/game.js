40fc35b6949285ba45f72a560fe28c8eb8f02eac
Marcel <marcel.riek@googlemail.com>
2020-01-16 18:50:17 +0100

    done

--- src/game.js
+++ src/game.js
@@ -1,7 +1,20 @@
+
 class Game {
   drawGrid() {
     // Iteration 1
     // Draw the grid
     // https://p5js.org/reference/#/p5/line
+    // stroke("black");
+    // strokeWeight(5);
+    // line(0, 0, 0, 1000);
+
+    for (var x = 0; x <= WIDTH; x += WIDTH / 10) {
+      for (var y = 0; y <= HEIGHT; y += HEIGHT / 10) {
+        stroke(0);
+        strokeWeight(1);
+        line(x, 0, x, HEIGHT);
+        line(0, y, WIDTH, y);
+      }
+    }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,107 @@
-const game = new Game();
+let downImage;
+let treasureImg;
 
+function preload() {
+  downImage = loadImage("./../assets/character-down.png");
+  treasureImg = loadImage("./../assets/treasure.png");
+}
+
+const game = new Game();
+// const player = new Player();
 function setup() {
   let canvas = createCanvas(WIDTH, HEIGHT);
   canvas.parent("canvas");
 }
-
 function draw() {
   game.drawGrid();
+  player.draw();
+  treasure.draw();
+  if (player.row === treasure.row && player.col === treasure.col) {
+    clear(treasure);
+    treasure.setRandomPosition();
+  }
 }
+
+class Player {
+  constructor(col, row) {
+    this.col = col;
+    this.row = row;
+  }
+
+  preload() {
+    this.downImage = downImage;
+  }
+
+  moveUp() {
+    if (this.row > 0) {
+      this.row -= SQUARE_SIDE;
+    }
+  }
+
+  moveDown() {
+    if (this.row < HEIGHT - SQUARE_SIDE) {
+      this.row += SQUARE_SIDE;
+    }
+  }
+
+  moveLeft() {
+    if (this.col > 0) {
+      this.col -= SQUARE_SIDE;
+    }
+  }
+
+  moveRight() {
+    if (this.col < WIDTH - SQUARE_SIDE) {
+      this.col += SQUARE_SIDE;
+    }
+  }
+
+  draw() {
+    image(downImage, this.col, this.row, 100, 100);
+  }
+}
+
+// Iteration 4:
+let player = new Player(100, 100);
+
+function keyPressed() {
+  // const step = 100;
+  if (keyCode === 38) {
+    // up arrow
+    player.moveUp();
+    // player.row -= step;
+  } else if (keyCode === 40) {
+    // down arrow
+    player.moveDown();
+  } else if (keyCode === 39) {
+    // right arrow
+    player.moveRight();
+  } else if (keyCode === 37) {
+    // left arrow
+    player.moveLeft();
+  }
+  clear(player);
+}
+
+class Treasure {
+  constructor() {
+    this.col = this.setRandomPosition();
+    this.row = this.setRandomPosition();
+  }
+
+  setRandomPosition() {
+    this.col = Math.floor(Math.random(WIDTH / 10) * 10) * 100;
+    this.row = Math.floor(Math.random(HEIGHT / 10) * 10) * 100;
+  }
+
+  preload() {
+    this.treasureImg = treasureImg;
+  }
+
+  draw() {
+    image(treasureImg, this.col, this.row, 100, 100);
+  }
+}
+let treasure = new Treasure();
+
+treasure.setRandomPosition();
