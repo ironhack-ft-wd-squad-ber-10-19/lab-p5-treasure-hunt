444450a38c83c48697cc381acb9a8fa2fa85a016
Alba-Tros <samipcpoolberlin@yahoo.fr>
2020-04-02 16:52:55 +0200

    Iteration 3 first stab

--- src/game.js
+++ src/game.js
@@ -11,31 +11,3 @@ class Game {
         }
     }
 }
-class Player {
-    constructor(col, row) {
-        this.col = col;
-        this.row = row;
-    }
-    moveUp() {
-        this.row--;
-        console.log("moveUp");
-    }
-    moveDown() {
-        this.row++;
-        console.log("moveDown");
-    }
-    moveLeft() {
-        this.col--;
-        console.log("moveLeft");
-    }
-    moveRight() {
-        this.col++;
-        console.log("moveRight");
-    }
-}
-
-const player = new Player(0, 0); // (0,0) = Initial position (col, row)
-player.moveDown(); // Increase by 1 the value of player.row
-player.moveDown(); // Increase by 1 the value of player.row
-player.moveRight(); // Increase by 1 the value of player.col
-console.log(player.col, player.row); // => 1,2
