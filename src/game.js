54eb79f61f127a749c673bedee7e7b16a90943e8
Eduarda Liu <eduardaliu@gmail.com>
2020-06-11 21:07:23 +0200

    done

--- src/game.js
+++ src/game.js
@@ -1,7 +1,73 @@
 class Game {
   drawGrid() {
-    // Iteration 1
-    // Draw the grid
-    // https://p5js.org/reference/#/p5/line
+    for (let x = 0; x <= WIDTH; x += WIDTH / 10) {
+      line(x, 0, x, HEIGHT);
+    }
+    for (let y = 0; y <= HEIGHT; y += HEIGHT / 10) {
+      line(0, y, WIDTH, y);
+      }
+    }
+    preload() {
+    this.player = new Player;
+    this.playerImgDefault = loadImage("./assets/character-down.png");
+    this.treasureImg = loadImage("./assets/treasure.png");
+    }
+}
+
+
+class Player {
+  constructor(col, row) {
+    this.col = col;
+    this.row = row;
+  }
+  moveUp() {
+    this.row-= 100;
+  }
+  moveDown() {
+    this.row+= 100;
+  }
+  moveLeft() {
+    this.col-= 100;
+  }
+  moveRight() {
+    this.col+= 100;
+  }
+  render () {
+    clear();
+    frameRate(10);
+    image(game.playerImgDefault, this.col, this.row, 100, 100);
+  }
+  keyPressed () {
+    if (keyIsDown(38)) {
+      game.playerImgDefault = loadImage("assets/character-up.png");
+      this.moveUp();
+      
+    }
+    if (keyIsDown(40)) {
+      game.playerImgDefault = loadImage("assets/character-down.png");
+      this.moveDown();
+    }
+    if (keyIsDown(37)) {
+      game.playerImgDefault = loadImage("assets/character-left.png");
+            this.moveLeft();
+    }
+    if (keyIsDown(39)) {
+      game.playerImgDefault = loadImage("assets/character-right.png");
+      this.moveRight();
+    }
+  }
+}
+
+class Treasure {
+  constructor(col, row) {
+    this.col = col;
+    this.row = row;
+  }
+  setRandomPosition() {
+    this.randomCol = Math.floor(Math.random()*9)*100;
+    this.randomRow = Math.floor(Math.random()*9)*100;
+  }
+  render() {
+    image(game.treasureImg, this.randomCol, this.randomRow, 100, 100);
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 const game = new Game();
+const player = new Player(0, 0);
+const treasure = new Treasure(100, 100);
+
+function preload() {
+  game.preload();
+  treasure.setRandomPosition();
+}
 
 function setup() {
   let canvas = createCanvas(WIDTH, HEIGHT);
@@ -6,5 +13,9 @@ function setup() {
 }
 
 function draw() {
+  player.render();
+  player.keyPressed();
   game.drawGrid();
+  treasure.render();
 }
+
