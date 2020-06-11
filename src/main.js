f204e8cab84ab421cefdddc0251725db516862a9
Romulo Galiao <Romulo.Galiao@googlemail.com>
2020-06-11 21:02:30 +0200

    done

--- src/main.js
+++ src/main.js
@@ -1,10 +1,36 @@
 const game = new Game();
 
 function setup() {
-  let canvas = createCanvas(WIDTH, HEIGHT);
-  canvas.parent("canvas");
+  let canvas = createCanvas(600, 600); 
+  canvas.parent("canvas");  // spricht die ID im HTML an 
 }
 
 function draw() {
   game.drawGrid();
-}
+  background(250); // hintergrung(Farbe);
+
+  for (var x = 0; x <= width; x += width / 10) {  
+  for (var y = 0; y <= height; y += height / 10) {
+    stroke(100);  // farbe
+    strokeWeight(5); // dicke der linien 
+    line(x, 0, x, height); 
+    line(0, y, width, y); 
+   }
+  }
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
