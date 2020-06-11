2a1222f15f543e739dad20b861ec1b58568dae7d
SiEr0212 <simonertel@gmx.de>
2020-06-11 23:14:42 +0200

    Basis functions done, player can move around

--- src/game.js
+++ src/game.js
@@ -3,5 +3,40 @@ class Game {
     // Iteration 1
     // Draw the grid
     // https://p5js.org/reference/#/p5/line
+    background(200);
+    for (let x = 0; x < WIDTH; x += WIDTH / 10) {
+      for (let y = 0; y < HEIGHT; y += HEIGHT / 10) {
+        strokeWeight(2);
+        line(x, 0, x, HEIGHT);
+        line(0, y, WIDTH, y);
+      }
+    }
   }
 }
+
+class Player {
+  constructor(col, row) {
+    this.col = col;
+    this.row = row;
+  }
+  drawPlayer() {
+    frameRate(10);
+    image(playerImg, this.col, this.row, 100, 100);
+  }
+
+  moveUp() {
+    this.row -= 100;
+  }
+
+  moveDown() {
+    this.row += 100;
+  }
+
+  moveLeft() {
+    this.col -= 100;
+  }
+  moveRight() {
+    this.col += 100;
+  }
+}
+
--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 const game = new Game();
+const player = new Player(100,100);
+//const player = new Player(0,0);
+
+function preload() {
+  
+playerImg = loadImage("assets/character-down.png");
+
+}
 
 function setup() {
   let canvas = createCanvas(WIDTH, HEIGHT);
@@ -6,5 +14,28 @@ function setup() {
 }
 
 function draw() {
+  clear();
   game.drawGrid();
+  player.drawPlayer();
+  
 }
+
+function keyPressed() {
+  if (keyCode == 38) {
+    player.moveUp();
+  }
+  if (keyCode == 40) {
+    player.moveDown();
+  }
+  if (keyCode == 37) {
+    player.moveLeft();
+  }
+  if (keyCode == 39) {
+    player.moveRight();
+  }
+  if (keyCode == 67) {
+    player.reset();
+  }
+}
+
+
