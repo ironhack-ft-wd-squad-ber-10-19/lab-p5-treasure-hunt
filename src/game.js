833d896ee4e20860f27872aa54b788ac340f82cd
Ivan Mickovski <ivan_mickovski@hotmail.com>
2020-06-11 18:55:49 +0200

    Done till Iteration 4, Needs fixing on position display on Iteration 5

--- src/game.js
+++ src/game.js
@@ -3,5 +3,52 @@ class Game {
     // Iteration 1
     // Draw the grid
     // https://p5js.org/reference/#/p5/line
+    background(220);
+    for(let x = 0; x <= WIDTH; x += WIDTH / 10) {
+      for(let y = 0; y <= HEIGHT; y += HEIGHT / 10) {
+        stroke(100);
+        strokeWeight(3);
+        line(x, 0, x, HEIGHT);
+        line(0, y, WIDTH, y)
+      }
+    }
+  }
+}
+
+
+
+class Player {
+  constructor(row, col) {
+    this.col = col
+    this.row = row
+  }
+  drawPlayer() {
+    image(mickoImg, this.row, this.col, 100, 100)
+  }
+  moveUp() {
+    this.col -= 100;
+  }
+  moveDown() {
+    this.col += 100;
+  }
+  moveLeft() {
+    this.row -= 100;
+  }
+  moveRight() {
+    this.row += 100;
+  }
+}
+
+class Treasure {
+  constructor(){
+    this.col = Math.floor(Math.random() * 1000)
+    this.row = Math.floor(Math.random() * 1000)
+  }
+  // setRandomPosition(){
+  //   this.col = Math.floor(Math.random() * 1000)
+  //   this.row = Math.floor(Math.random() * 1000)
+  // }
+  drawTreasure() {
+    image(treasureImg, this.col, this.row, 100, 100)
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 const game = new Game();
+const micko = new Player(400, 400);
+const gold = new Treasure()
+
+
+function preload() {
+  mickoImg = loadImage("assets/character-down.png");
+  treasureImg = loadImage("assets/treasure.png");
+}
 
 function setup() {
   let canvas = createCanvas(WIDTH, HEIGHT);
@@ -6,5 +14,28 @@ function setup() {
 }
 
 function draw() {
+  clear();
   game.drawGrid();
+  micko.drawPlayer();
+  gold.drawTreasure();
 }
+
+function keyPressed() {
+  if (keyCode == 38) {
+    micko.moveUp();
+  }
+  if (keyCode == 40) {
+    micko.moveDown();
+  }
+  if (keyCode == 37) {
+    micko.moveLeft();
+  }
+  if (keyCode == 39) {
+    micko.moveRight();
+  }
+  if (keyCode == 67) {
+      micko.reset();
+  }
+}
+
+
