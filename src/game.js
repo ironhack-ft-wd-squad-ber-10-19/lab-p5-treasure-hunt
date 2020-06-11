5723bf109ec0e03979445abade6c8fe7082209e9
MRzsztk <rzeszutek.malgorzata@gmail.com>
2020-06-11 18:50:47 +0200

    player problem

--- src/game.js
+++ src/game.js
@@ -3,5 +3,32 @@ class Game {
     // Iteration 1
     // Draw the grid
     // https://p5js.org/reference/#/p5/line
+    for (let i=0; i<=10; i++) {
+      line(SQUARE_SIDE*i, 0, SQUARE_SIDE*i, HEIGHT)
+      line(0, SQUARE_SIDE*i, WIDTH, SQUARE_SIDE*i)
+    }
   }
+}
+let playerImg = loadImage("./assets/character-down.png");
+class Player {
+  constructor(col, row) {
+    this.column = col;
+    this.row = row;
+  }
+    moveUp() {
+      this.row++
+    }
+    moveDown() {
+      this.row--
+    }
+    moveLeft() {
+      this.column--
+    }
+    moveRight() {
+      this.column++
+    }
+    draw() {
+      image(this.playerImg, this.row, this.column, SQUARE_SIDE, SQUARE_SIDE);
+    }
+  
 }
