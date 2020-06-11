32779a50cc2d96002566ca33ea3fa7078ee9bdba
Romulo Galiao <Romulo.Galiao@googlemail.com>
2020-06-11 23:22:56 +0200

    iteration 3 done

--- src/game.js
+++ src/game.js
@@ -1,8 +1,30 @@
 class Game {
   drawGrid() {
     // Iteration 1
-    
-    // Draw the grid
+    background(250); // hintergrung(Farbe);
+
+  for (var x = 0; x <= width; x += width / 10) {  
+  for (var y = 0; y <= height; y += height / 10) {
+    stroke(100);  // farbe
+    strokeWeight(5); // dicke der linien 
+    line(x, 0, x, height); 
+    line(0, y, width, y); 
+   }
+  }
+
     // https://p5js.org/reference/#/p5/line
   }
+  pleaseLoadGameImages() {
+    // we load gustavo before using its variables in the doSomething function
+    this.rom = new Player(10, 0);
+
+    // we use loadImage to load the images BEFORE actually using it
+    this.romImg = loadImage("assets/character-down.png"); 
+  }
+  doSomething() {
+    console.log(this.rom.col)
+    image(this.romImg, this.rom.col,this.rom.row);
 }
+}
+
+
--- src/main.js
+++ src/main.js
@@ -1,36 +1,55 @@
 const game = new Game();
 
+function preload() {
+  romImg = loadImage("assets/character-down.png");
+  treasureImg = loadImage("assets/treasure.png");
+}
+
 function setup() {
   let canvas = createCanvas(600, 600); 
-  canvas.parent("canvas");  // spricht die ID im HTML an 
+  canvas.parent("canvas");  // spricht die ID im HTML an
 }
 
 function draw() {
+  //game.pleaseLoadGameImages();
+
+  //game.doSomething()
   game.drawGrid();
-  background(250); // hintergrung(Farbe);
+  player.drawPlayer();
+}
 
-  for (var x = 0; x <= width; x += width / 10) {  
-  for (var y = 0; y <= height; y += height / 10) {
-    stroke(100);  // farbe
-    strokeWeight(5); // dicke der linien 
-    line(x, 0, x, height); 
-    line(0, y, width, y); 
-   }
+class Player {
+  constructor(x,y){
+    this.col = x; 
+    this.row = y ;
+    //console.log(col,row)
   }
-	}
+  drawPlayer() {
+    frameRate(10);
+    image(romImg, this.row, this.col, 60, 60)
+  }
+
+  moveUp() {
+    this.col -= 60;
+  }
+
+  moveDown() {
+    this.col += 60;
+  }
+  moveLeft() {
+    this.row -= 60;
+  }
+  moveRight() {
+    this.row += 60;
+  }
+
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+ const player = new Player(0,0) // (0,0) = Initial position (col, row)
+ player.moveDown() // Increase by 1 the value of player.row
+// player.moveDown() // Increase by 1 the value of player.row
+// player.moveRight() // Increase by 1 the value of player.col
+// console.log(player.col, player.row) // => 1,2
 
 
