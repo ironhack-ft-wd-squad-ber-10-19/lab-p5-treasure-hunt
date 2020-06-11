6763952f4722ba432a5f76346ff1e36de7cc3f3d
Leonardo Costa <leo.costa.programming@gmail.com>
2020-06-11 21:37:17 +0200

    BER WDFT - Leo Costa - not fully working game

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 const game = new Game();
+const player = new Player(400, 400);
+const gold = new Treasure()
+
+
+function preload() {
+  characterImg = loadImage("assets/character-down.png");
+  treasureImg = loadImage("assets/treasure.png");
+}
 
 function setup() {
   let canvas = createCanvas(WIDTH, HEIGHT);
@@ -6,5 +14,26 @@ function setup() {
 }
 
 function draw() {
+  clear();
   game.drawGrid();
+  player.drawPlayer();
+  gold.drawTreasure();
+}
+
+function keyPressed() {
+  if (keyCode == 38) {
+    player.moveUp();
+  }
+  if (keyCode == 40) {
+    player.moveDown();
+  }
+  if (keyCode == 37) {
+    player.moveLeft();
+  }
+  if (keyCode == 39) {
+    player.moveRight();
+  }
+  if (keyCode == 67) {
+    player.reset();
+  }
 }
