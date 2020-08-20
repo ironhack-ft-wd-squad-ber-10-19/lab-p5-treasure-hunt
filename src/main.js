d8e36874d50153fe1bf0173b7126c0ff6518ab98
danparry <dan.parry22@gmail.com>
2020-08-20 18:07:46 +0200

    iteration 1-3

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 const game = new Game();
 
+function preload(){
+  game.preloadGame()
+}
+
 function setup() {
   let canvas = createCanvas(WIDTH, HEIGHT);
   canvas.parent("canvas");
+  game.setupGame()
 }
 
 function draw() {
-  game.drawGrid();
+  game.drawGame();
 }
