3e55e0a722b8c445a713e83dccecfea8878c2d17
Daniel Papastratidis <idanpap@gmail.com>
2020-08-20 20:14:49 +0200

    finished all except bonus

--- src/main.js
+++ src/main.js
@@ -1,10 +1,34 @@
 const game = new Game();
 
+
+function preload() {
+  game.preloadGame();
+}
+ 
 function setup() {
   let canvas = createCanvas(WIDTH, HEIGHT);
   canvas.parent("canvas");
+  game.setupGame()
 }
 
 function draw() {
   game.drawGrid();
+  game.drawGame()
+}
+
+function keyPressed() {
+  clear()
+  if (keyCode === 38) {
+    game.player.moveUp();
+  }
+  if (keyCode === 40) {
+    game.player.moveDown();
+  }
+  if (keyCode === 37) {
+    game.player.moveLeft();
+  }
+
+  if (keyCode === 39) {
+    game.player.moveRight();
+  }
 }
