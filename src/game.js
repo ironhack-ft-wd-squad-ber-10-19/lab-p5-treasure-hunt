6ad0a824b78eb3e5b257bba99e330ef8c17dfe29
Tolga Kara <tolgakara2.0@gmail.com>
2020-08-21 16:14:55 +0200

    done

--- src/game.js
+++ src/game.js
@@ -1,7 +1,32 @@
 class Game {
-  drawGrid() {
-    // Iteration 1
-    // Draw the grid
-    // https://p5js.org/reference/#/p5/line
-  }
+	drawGrid() {
+		// Iteration 1
+		// Draw the grid
+		// https://p5js.org/reference/#/p5/line
+
+		for (let grid = 0; grid <= 1000; grid += 100) {
+			line(0, grid, 1000, grid)
+			line(grid, 0, grid, 1000)
+		}
+	}
+	preloadGame() {
+		this.treasureImage = loadImage("/assets/treasure.png")
+		this.playerImage = loadImage("/assets/character-down.png")
+	}
+
+	setupGame() {
+		this.treasure = new Treasure(500, 500)
+		this.treasure.setRandomPosition()
+		this.treasure.image = this.treasureImage
+		this.player = new Player(0, 0)
+		this.player.image = this.playerImage
+	}
+
+	drawGame() {
+		this.treasure.drawTreasure()
+		this.player.drawPlayer()
+		if (this.player.col === this.treasure.col && this.player.row === this.treasure.row) {
+			this.treasure.setRandomPosition()
+		}
+	}
 }
---
+++ src/player.js
@@ -0,0 +1,38 @@
+class Player {
+	constructor(col, row) {
+		this.col = col
+		this.row = row
+		this.image
+	}
+
+	setupPlayer() {}
+
+	moveUp() {
+		if (this.row > 0) {
+			this.row -= 100
+			this.image = loadImage("/assets/character-up.png")
+		}
+	}
+	moveDown() {
+		if (this.row < 900) {
+			this.row += 100
+			this.image = loadImage("/assets/character-down.png")
+		}
+	}
+	moveLeft() {
+		if (this.col > 0) {
+			this.col -= 100
+			this.image = loadImage("/assets/character-left.png")
+		}
+	}
+	moveRight() {
+		if (this.col < 900) {
+			this.col += 100
+			this.image = loadImage("/assets/character-right.png")
+		}
+	}
+
+	drawPlayer() {
+		image(this.image, this.col, this.row, 100, 100)
+	}
+}
