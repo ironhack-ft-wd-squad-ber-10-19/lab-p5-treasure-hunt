dd74923e34eb3074920caf6a0edbde9cffb76186
Marly <marlenak90@gmail.com>
2020-08-23 16:46:30 +0200

    score updated in HTML

--- src/game.js
+++ src/game.js
@@ -3,6 +3,7 @@ class Game {
   constructor(){
     this.treasure = new Treasure ();
     this.player = new Player ();
+    this.player2 = new Player ();
   }
 
   preloadGame() {
@@ -25,15 +26,23 @@ class Game {
     // https://p5js.org/reference/#/p5/line
     
     clear ();
+    const scoreCount = document.querySelector('.score');
     for (let i=0; i<WIDTH+SQUARE_SIDE;i+=SQUARE_SIDE) {
       console.log(i);
       push();
       line(0, i, WIDTH, i);
       line(i, 0, i, WIDTH);
       pop ();
+      if (this.treasure.col === this.player.col && this.treasure.row === this.player.row) {
+        this.treasure.setRandomPosition();
+        this.player.score+=1;
+      scoreCount.innerHTML = `Score: ${this.player.score}`
+      }
     }
     
     this.player.drawPlayer();
     this.treasure.drawTreasure();
   }
+
+
 }
--- src/player.js
+++ src/player.js
@@ -1,24 +1,29 @@
 class Player {
     constructor() {
-        this.col =0;
-        this.row =0;
+        this.col=0;
+        this.row=0;
         this.image;
+        this.score=0;
     }
         moveUp () {
             (this.col >0) ? this.col-=SQUARE_SIDE: this.col-=0;
             this.image = loadImage('./assets/character-up.png');
+            console.log('new position is',this.row,this.col);
         }
         moveDown (){
             (this.col < HEIGHT-SQUARE_SIDE) ? this.col+=SQUARE_SIDE : this.col+=0;
             this.image = loadImage('./assets/character-down.png');
+            console.log('new position is',this.row,this.col);
         }
         moveLeft(){
             (this.row >0) ? this.row-= SQUARE_SIDE : this.row-=0;
             this.image = loadImage('./assets/character-left.png');
+            console.log('new position is',this.row,this.col);
         }
         moveRight(){
             (this.row <WIDTH-SQUARE_SIDE) ? this.row+= SQUARE_SIDE : this.row+= 0;
-            this.image = loadImage('./assets/character-right.png');    
+            this.image = loadImage('./assets/character-right.png');
+            console.log('new position is',this.row,this.col);
         }
 
     preloadPlayer (){
@@ -41,6 +46,8 @@ class Player {
         keyIsDown(DOWN_ARROW) ? game.player.moveDown() : this.col+=0;
         keyIsDown(LEFT_ARROW) ? game.player.moveLeft() : this.row-=0;
         keyIsDown(RIGHT_ARROW) ? game.player.moveRight() : this.row+=0;
-    }
 
+        
+    }
+    
 }
