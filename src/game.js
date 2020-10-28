50ffbe7bbd57bf1f2460484192843d3670a93ff0
“Anna <aniawaszak@yahoo.com>
2020-10-28 20:09:36 +0100

    done

--- src/game.js
+++ src/game.js
@@ -1,7 +1,82 @@
 class Game {
+
+  preLoadGame() {
+    this.playerImage = loadImage("../assets/character-down.png");
+    this.treasureImage = loadImage("../assets/treasure.png");
+  }
+
+  setupGame() {
+    this.player = new Player(0,0);
+    this.player.image = this.playerImage;
+    this.treasure = new Treasure();
+    this.treasure.image = this.treasureImage;
+  }
+
   drawGrid() {
-    // Iteration 1
-    // Draw the grid
-    // https://p5js.org/reference/#/p5/line
+    let col = 10;
+    let row = 10;
+    background(230);
+  
+    for (let i = 0; i < col; i++) {
+      for (let j = 0; j < row; j++) {
+        let x = i * 100;
+        let y = j * 100;
+        stroke(0);
+        fill(255);
+        rect(x, y, 100, 100);
+      }
+    }
+  }
+
+  drawGame() {
+    this.player.drawPlayer();
+    this.treasure.drawTreasure();
+  }
+}
+
+
+class Player {
+  constructor(col, row) {
+    this.col = col;
+    this.row = row;
+    this.image;
+  }
+  moveUp() {
+    if (this.row > 0) {
+      this.row -= 100;
+    }
+  }
+  moveDown() {
+    if (this.row < 900) {
+      this.row += 100;
+    }
+  }
+  moveLeft() {
+    if (this.column < 0) {
+      this.col -= 100;
+    }
+  }
+  moveRight() {
+    if (this.column > 900) {
+      this.col += 100;
+    }
+  }
+  drawPlayer() {
+    image(this.image, this.col, this.row, 100, 100);
+  }
+}
+
+class Treasure {
+  constructor() {
+    this.col = Math.floor(Math.random() * 10) * 100;
+    this.row = Math.floor(Math.random() * 10) * 100;
+    this.image;
+  }
+  // setRandomPosition() {
+  //   this.col = Math.floor(Math.random() * 10) * 100;
+  //   this.row = Math.floor(Math.random() * 10) * 100;
+  // }
+  drawTreasure() {
+    image(this.image, this.col, this.row, 100, 100);
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,34 @@
 const game = new Game();
 
+function preload() {
+  game.preLoadGame();
+}
+
 function setup() {
   let canvas = createCanvas(WIDTH, HEIGHT);
   canvas.parent("canvas");
+  game.setupGame();
 }
 
 function draw() {
   game.drawGrid();
+  game.drawGame();
 }
+
+function keyPressed() {
+  if (keyIsDown(LEFT_ARROW)) {
+    game.player.col -= 100;
+  }
+  if (keyIsDown(RIGHT_ARROW)) {
+    game.player.col += 100;
+  }
+  if (keyIsDown(UP_ARROW)) {
+    game.player.row -= 100;
+  }
+  if (keyIsDown(DOWN_ARROW)) {
+    game.player.row += 100;
+  }
+}
+
+
+
