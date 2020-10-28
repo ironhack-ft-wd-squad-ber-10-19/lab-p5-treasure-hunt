b2f382bb55a83801dff21ff7a55308f72481237f
Mark Keeble <mkrangsdorf-ironhack@yahoo.com>
2020-10-28 20:20:29 +0100

    Done

--- src/main.js
+++ src/main.js
@@ -1,10 +1,27 @@
 const game = new Game();
+const treasure = new Treasure()
+const player = new Player(0,0);
 
+function preload(){
+}
 function setup() {
   let canvas = createCanvas(WIDTH, HEIGHT);
   canvas.parent("canvas");
+  treasure.preload()
+  player.preload()
 }
 
 function draw() {
+  clear();
   game.drawGrid();
+  treasure.draw();
+  player.draw();
 }
+
+function keyPressed() {
+  if (keyCode === 38) player.moveUp();
+  if (keyCode === 40) player.moveDown();
+  if (keyCode === 37) player.moveLeft();
+  if (keyCode === 39) player.moveRight();
+}
+
