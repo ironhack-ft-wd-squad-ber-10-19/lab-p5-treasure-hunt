1eccc4da305d59ab81d59323aa1b4bc13b2193db
prullansky <owenchboy@gmail.com>
2020-10-28 20:10:01 +0100

    Cant for the love of Satan make the character appear in the grid

--- src/game.js
+++ src/game.js
@@ -1,7 +1,59 @@
 class Game {
+
   drawGrid() {
-    // Iteration 1
-    // Draw the grid
-    // https://p5js.org/reference/#/p5/line
+  for (let x = 0; x <= width; x += width / 10) {
+		for (let y = 0; y <= height; y += height / 10) {
+			stroke(0);
+			strokeWeight(1);
+			line(x, 0, x, height);
+			line(0, y, width, y);
+		}
+	}
   }
+}
+
+class Player {
+  constructor(row,col) {
+    this.col = col;
+    this.row = row;
+    this.image = loadImage('../assets/character-down.png');
+  }
+
+  moveUp(){
+      this.row += 1;
+      this.image = loadImage('../assets/character-up.png');
+  }
+
+  moveDown(){
+      this.row -= 1;
+      this.image = loadImage('../assets/character-down.png');
+  }
+
+  moveLeft(){
+      this.col -= 1;
+      this.image = loadImage('../assets/character-left.png');
+  }
+
+  moveRight(){
+      this.col += 1;
+      this.image = loadImage('../assets/character-right.png');
+  }
+
+  drawPlayer() {
+    image(this.image,this.col*SQUARE_SIDE,this.row*SQUARE_SIDE,SQUARE_SIDE,SQUARE_SIDE);
+  }
+}
+
+class Treasure {
+  constructor(row,col) {
+    this.col = col;
+    this.row = row;
+    this.image = loadImage('../assets/treasure.png');
+  }  
+
+  setRandomPosition() {
+
+  }
+
+
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,25 @@
 const game = new Game();
+const player = new Player(0,0);
+const treasure = new Treasure();
 
 function setup() {
   let canvas = createCanvas(WIDTH, HEIGHT);
   canvas.parent("canvas");
+
+}
+
+function preload(){
+  player.(this.image);
 }
 
 function draw() {
   game.drawGrid();
+  player.draw();
+}
+
+function keyPressed() {
+  if (keyCode === 38) player.moveUp();
+  if (keyCode === 40) player.moveDown();
+  if (keyCode === 37) player.moveLeft();
+  if (keyCode === 39) player.moveRight();
 }
