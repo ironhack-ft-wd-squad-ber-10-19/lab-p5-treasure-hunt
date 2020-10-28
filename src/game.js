69a9908760c5986be448133cc21dc59beca177b8
Katie Gracey <gracey.kd@gmail.com>
2020-10-28 19:48:52 +0100

    Complete basic iterations

--- src/game.js
+++ src/game.js
@@ -1,7 +1,61 @@
 class Game {
   drawGrid() {
-    // Iteration 1
-    // Draw the grid
-    // https://p5js.org/reference/#/p5/line
+    for (var x = 0; x <= WIDTH; x += SQUARE_SIDE) {
+      for (var y = 0; y <= HEIGHT; y += SQUARE_SIDE) {
+        line(x, 0, x, HEIGHT);
+        line(0, y, WIDTH, y);
+      }
+    }
+  }
+}
+
+class Player {
+  constructor(row, col) {
+    this.row = 0;
+    this.col = 0;
+    this.image;
+  }
+
+  preload(){
+    this.image = loadImage('/assets/character-down.png');
+  }
+
+  drawPlayer() {
+    image(this.image, this.row*SQUARE_SIDE, this.col*SQUARE_SIDE, SQUARE_SIDE, SQUARE_SIDE);
+  }
+
+  moveUp() {
+    this.col -= 1;
+  }
+
+  moveDown() {
+    this.col += 1;
+  }
+  moveLeft() {
+    this.row -= 1;
+  }
+  moveRight() {
+    this.row += 1;
+  }  
+}
+
+class Treasure {
+  constructor(row, col){
+    this.row = 0;
+    this.col = 0;
+    this.image;
+  }
+
+  setRandomPosition(){
+    this.row = Math.floor(Math.random()*10) ;
+    this.col = Math.floor(Math.random()*10) ;
+  }
+
+  preload(){
+    this.image = loadImage('/assets/treasure.png');
+  }
+
+  drawTreasure(){
+    image(this.image, this.row*SQUARE_SIDE, this.col*SQUARE_SIDE, SQUARE_SIDE, SQUARE_SIDE);
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,37 @@
 const game = new Game();
+const player = new Player();
+const treasure = new Treasure();
+
 
 function setup() {
-  let canvas = createCanvas(WIDTH, HEIGHT);
+  let canvas = createCanvas(HEIGHT, WIDTH);
   canvas.parent("canvas");
 }
 
+function preload(){
+  player.preload();
+  treasure.setRandomPosition();
+  treasure.preload();
+}
+
 function draw() {
+  clear();
   game.drawGrid();
+  player.drawPlayer();
+  treasure.drawTreasure();
+}
+
+function keyPressed(){
+  if (keyCode === 40) {
+    player.moveDown();
+  }
+  if (keyCode === 38) {
+   player.moveUp();
+  }
+  if (keyCode === 37) {
+    player.moveLeft();
+  }
+  if (keyCode === 39) {
+    player.moveRight();
+  }
 }
