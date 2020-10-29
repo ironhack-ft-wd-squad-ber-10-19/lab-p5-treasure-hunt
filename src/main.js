3bc603158c23785a17b05f3f6369485f7746885f
lighteronn <mezora197@gmail.com>
2020-10-29 01:01:04 +0100

    done

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 const game = new Game();
+let img;
 
+
+function preload() {
+  img = loadImage('assets/character-down.png')
+}
 function setup() {
   let canvas = createCanvas(WIDTH, HEIGHT);
   canvas.parent("canvas");
-}
-
+};
 function draw() {
   game.drawGrid();
+  image(img, 0, 0);
 }
+
+
